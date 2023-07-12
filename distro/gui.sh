5f4b9bbe2013cff1279332f0ec30dcb353dc2198
MAXKROPKAXYZ <121354620+MAXKROPKAXYZ@users.noreply.github.com>
2023-07-12 15:54:02 +0200

    gui.sh

--- distro/gui.sh
+++ distro/gui.sh
@@ -288,4 +288,4 @@ install_softwares
 config
 note
 
-        packs=(sudo gnupg2 curl nano git xz-utils at-spi2-core xfce4 xfce4-goodies xfce4-terminal librsvg2-common menu inetutils-tools dialog exo-utils tigervnc-standalone-server tigervnc-common tigervnc-tools dbus-x11 fonts-beng fonts-beng-extra gtk2-engines-murrine gtk2-engines-pixbuf apt-transport-https)
+packs=(sudo gnupg2 curl nano git xz-utils lightdm at-spi2-core librsvg2-common menu xfce4-terminal ubuntu-desktop inetutils-tools dialog exo-utils tigervnc-standalone-server tigervnc-common tigervnc-tools dbus-x11 fonts-beng fonts-beng-extra gtk2-engines-murrine gtk2-engines-pixbuf apt-transport-https)
